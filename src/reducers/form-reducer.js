9664773b958442305639871e2a7cd6fbd7aef59c
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-22 13:31:08 +0300

    update the reducer function

--- src/reducers/form-reducer.js
+++ src/reducers/form-reducer.js
@@ -11,43 +11,24 @@ export const initialFormState = {
 
 export const formReducer = (state, action) => {
   if (action.type === "EMAIL_CHANGED") {
-    if (action.payload === "" || !action.payload.includes("@")) {
-      return {
-        ...state,
-        email: {
-          valid: false,
-          value: action.payload,
-        },
-      };
-    } else {
-      return {
-        ...state,
-        email: {
-          valid: true,
-          value: action.payload,
-        },
-      };
-    }
+    return {
+      ...state,
+      email: {
+        valid: action.payload !== "" && action.payload.includes("@"),
+        value: action.payload,
+      },
+    };
   }
 
   if (action.type === "PASSWORD_CHANGED") {
-    if (action.payload.trim().length < 7) {
-      return {
-        ...state,
-        password: {
-          valid: false,
-          value: action.payload,
-        },
-      };
-    } else {
-      return {
-        ...state,
-        password: {
-          valid: true,
-          value: action.payload,
-        },
-      };
-    }
+    return {
+      ...state,
+      password: {
+        valid: action.payload.trim().length > 7,
+        value: action.payload,
+      },
+    };
   }
+
   return initialFormState;
 };
